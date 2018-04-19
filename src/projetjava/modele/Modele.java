817adcda4980f65ae102808247dc284347a7c8b4
Fabrice-vega <fabrice.vega@outlook.com>
2018-04-20 00:01:23 +0200

    Modifications(1) 20/04/18

--- src/projetjava/modele/Modele.java
+++ src/projetjava/modele/Modele.java
@@ -17,10 +17,21 @@ import projetjava.modele.Classes;
  */
 public class Modele {
     
+    /**
+     * Liste des enseignants
+     */
     protected List<Enseignant> mesEnseignants = new ArrayList<>();
+
+    /**
+     * Liste des classes
+     */
     protected List<Classes> mesClasses = new ArrayList<>();
     
     //private static Modele instance = null;
+
+    /**
+     * Constructeur par défaut
+     */
     public Modele() {
         
     }
@@ -29,6 +40,13 @@ public class Modele {
         if(instance == null) return instance = new Modele();
         else return instance;
     }*/
+
+    /**
+     * Méthode qui ajoute un enseignant
+     * @param e l'enseignant à ajouter
+     * @return l'état de l'ajout
+     */
+
     
     public String ajoutEnseignant(Enseignant e) {
         if(e == null) return "enseignant nul";
@@ -37,6 +55,11 @@ public class Modele {
         return "ajout enseignant effectuée";
     }
     
+    /**
+     * Méthode qui ajoute une classe
+     * @param c la classe à ajouter
+     * @return l'état de la classe
+     */
     public String ajoutClasses(Classes c) {
         if(c == null) return "Classe nul";
         if(mesClasses.contains(c)) return "classe déjà enregistrée";
@@ -44,55 +67,131 @@ public class Modele {
         return "ajout classe effectuée";
     }
     
+    /**
+     * Méthode qui compare les enseignants
+     * @return la liste des enseignants
+     */
     public List<Enseignant> tousEnseignant() {
         mesEnseignants.sort(new CompareEnseignant());
         return mesEnseignants;
     }
     
+    /**
+     * Méthode qui compare les classes
+     * @return la liste des classes
+     */
     public List<Classes> toutesClasses() {
         mesClasses.sort(new CompareClasses());
         return mesClasses;
     }
     
+    /**
+     * Méthode qui accède à un enseignant
+     * @param idRech objet de type Enseignant qui sert à trouver l'élément cherché
+     * @return l'objet de type Enseignant
+     */
     public Enseignant getEnseignant(Enseignant idRech) {
         int i = mesEnseignants.indexOf(idRech);
         if(i<0) return null;
         else return mesEnseignants.get(i);
     }
     
-    public Classes getClasse(Classes sigleRech) {
+    /**
+     * Méthode qui accède à une classe
+     * @param sigleRech objet de type Classes qui sert à trouver l'élément cherché
+     * @return m'objet de type Classes
+     */
+    public Classes getClasse(String sigleRech) {
         int i = mesClasses.indexOf(sigleRech);
         if(i < 0) return null;
         else return mesClasses.get(i);
     }
     
+    /**
+     * Méthode qui modifie le nom
+     * @param e objet de type Enseignant
+     * @param nom objet qui reçoit le nouveau nom
+     * @return l'état de l'objet
+     */
     public String modifNom(Enseignant e, String nom) {
         e.setNom(nom);
         return "Changement de nom effectué";
     }
     
+    /**
+     * Méthode qui modifie le prénom
+     * @param e objet de type Enseignant
+     * @param prenom objet qui reçoit le nouveau prénom
+     * @return l'état de l'objet
+     */
     public String modifPrenom(Enseignant e, String prenom) {
         e.setPrenom(prenom);
         return "Changement de prénom effectué";
     }
     
+    /**
+     * Méthode qui modifie l'id du prof
+     * @param e objet de type Enseignant
+     * @param id_prof objet qui reçoit le nouvel id
+     * @return l'état de l'objet
+     */
     public String modifIdProf(Enseignant e, String id_prof) {
         e.setId_prof(id_prof);
         return "Changement de d'id effectué";
     }
     
+    /**
+     * Méthode qui modifie le sigle
+     * @param c objet de type Classes
+     * @param sigle objet qui reçoit le nouveau sigle
+     * @return l'état de l'objet
+     */
     public String modifSigle(Classes c, String sigle) {
         c.setSigle(sigle);
         return "Changement du sigle effectué";
     }
     
+    /**
+     * Méthode qui modifie l'orientation
+     * @param c objet de type Classes
+     * @param orientation objet qui reçoit la nouvelle orientation
+     * @return l'état de l'objet
+     */
     public String modifOrientation(Classes c, String orientation) {
         c.setOrientation(orientation);
         return "Changement de l'orientation effectué";
     }
     
+    /**
+     * Méthode qui modifie l'année
+     * @param c objet de type Classes
+     * @param annee objet qui reçoit la nouvelle année
+     * @return l'état de l'objet
+     */
     public String modifAnnee(Classes c, int annee) {
         c.setAnnee(annee);
         return "Changement de l'année effectuée";
+    }
+    
+    /**
+     * Méthode qui supprime une classe
+     * @param c objet de type Classes à supprimer
+     * @return l'état de l'objet
+     */
+    public String supprClasse(Classes c) {
+        boolean ok = mesClasses.remove(c);
+        if(ok) return "Classe supprimée";
+        else return "Classe introuvable ou impossible à supprimer";
+    }
+    
+    /**
+     * Méthode qui supprime un enseignants
+     * @param e objet de type Enseignement à supprimer
+     * @return l'état de l'objet
+     */
+    public String supprEnseignant(Enseignant e) {
+        boolean ok = mesEnseignants.remove(e);
+        if(ok) return "Enseignant supprimé";
+        else return "Enseignant introuvable ou impossible à supprimer";
     }
 }
